15a512e9d5f021236ee435fe31ae13c1fc18bc0b
ASHOK KUMAR <pathakamuriashok@gmail.com>
2026-01-01 21:31:33 +0530

    New Portfolio_001

---
+++ AK-Portfolio-Static/src/main/java/AK_Portfolio_Static/AK_Portfolio_Static/App.java
@@ -0,0 +1,13 @@
+package AK_Portfolio_Static.AK_Portfolio_Static;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
